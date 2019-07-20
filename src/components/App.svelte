ed892fb57114974bb1336bc0684c87d3f177df9b
srzvan <sbingu.razvan@gmail.com>
2019-07-20 19:49:08 +0300

    liveshare tests

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,11 +1,13 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+    text-decoration: underline;
+    font-size: 16px;
+  }
 </style>
 
 <h1>Hello {name}!</h1>
